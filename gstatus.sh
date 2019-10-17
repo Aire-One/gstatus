041d11c43732cbd9b286dc3a0b2fbd6293fa1dd6
Aire-One <erwan.maugere@gmail.com>
2019-10-17 17:40:58 +0200

    Manage multiple remotes with the same branch.

--- gstatus.sh
+++ gstatus.sh
@@ -6,10 +6,12 @@
 # No args for now, maybe some shortcuts in the future o/
 
 
+# Gives the current branch
 function git-current-branch {
     git rev-parse --symbolic-full-name --abbrev-ref HEAD --sq
 }
 
+# Gives the upstream branch
 function git-upstream-branch {
     git rev-parse --symbolic-full-name --abbrev-ref HEAD@{u} &>/dev/null
     if [ $? -eq 0 ] ; then
@@ -17,12 +19,47 @@ function git-upstream-branch {
     fi
 }
 
-function git-diff-ahead {
-    git cherry -v | wc -l
+# Find remotes with the same branch
+function git-remote-branches {
+    current=$(git-current-branch)
+    for i in $(git branch -r) ; do
+        if [[ $i == *$current ]] ; then
+            echo $i
+        fi
+    done
 }
 
+# Gives the number of commit ahead from upstream
+# $1 = remote to check
+function git-diff-ahead {
+    git cherry -v $1 | wc -l
+}
+
+# Gives the number of commit behind upstream
+# $1 = remote to check
 function git-diff-behind {
-    git log HEAD..$(git-upstream-branch) --oneline | wc -l
+    git log HEAD..$1 --oneline | wc -l
+}
+
+# Print the branching status for a given remote
+function branching-status {
+    # f126 is the unicone character for git branch icon from Font Awesome
+    printf " \033[0;33m\uf126 $1"
+
+    # diff
+    # Font Awesome icons :
+    #   f0fe=plus-square ; f146=minus-square ; f14a=check-square ;
+    #   ufd3= times-square
+    #   f055=plus-circle ; f056=minus-cicle ; f058=check-square ;
+    #   f057=times-circle
+    add=$(git-diff-ahead $1)
+    sub=$(git-diff-behind $1)
+
+    printf ' '
+    # [ $add != 0 ] && [ $sub != 0 ] && printf "\033[0m \033[0;33m\uf057 : "
+    [ $add != 0 ] && printf "\033[0m\033[0;32m\uf055 $add"
+    [ $sub != 0 ] && printf "\033[0m\033[0;31m\uf056 $sub"
+    [[ $add == 0 && $sub == 0 ]] && printf "\033[0;34m\uf058"
 }
 
 # Give a string with the current braching status :
@@ -32,32 +69,19 @@ function git-diff-behind {
 #     UPSTREAM : (italic yellow) The upstream branch
 #     DIFF : Diff with upstream (show icons and commits count behind/ahead or
 #       a OK symbol if aligned)
-function branching-status {
+function git-branching-status {
     printf "\033[1;34m$(git-current-branch)"
 
     upstream=$(git-upstream-branch)
-
     if [[ -n $upstream ]] ; then
-        # f126 is the unicone character for git branch icon from Font Awesome
-        printf '\033[0;33m \uf126 '
-
-        printf "\033[3;33m$upstream"
-
-        # diff
-        # Font Awesome icons :
-        #   f0fe=plus-square ; f146=minus-square ; f14a=check-square ;
-        #   ufd3= times-square
-        #   f055=plus-circle ; f056=minus-cicle ; f058=check-square ;
-        #   f057=times-circle
-        add=$(git-diff-ahead)
-        sub=$(git-diff-behind)
-
-        printf " "
-        # [ $add != 0 ] && [ $sub != 0 ] && printf "\033[0m \033[0;33m\uf057 : "
-        [ $add != 0 ] && printf "\033[0m\033[0;32m\uf055 $add "
-        [ $sub != 0 ] && printf "\033[0m\033[0;31m\uf056 $sub "
-        [[ $add == 0 && $sub == 0 ]] && printf "\033[0;34m\uf058"
+        branching-status $upstream
     fi
+
+    for i in $(git-remote-branches) ; do
+        if [[ $i == $upstream ]] ; then continue; fi
+        printf ' |'
+        branching-status $i
+    done
 
     # standard formating + chariage return at EOL ;)
     printf '\033[0;00m\n'
@@ -73,7 +97,6 @@ function git-stash-status {
     git stash list
 }
 
-branching-status
+git-branching-status
 git-status
 git-stash-status
-
